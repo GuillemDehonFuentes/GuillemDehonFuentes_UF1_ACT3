848e3e8ea6aee694eb29aa6a10364af6f322f3d0
GuillemDehonFuentes <156358165+GuillemDehonFuentes@users.noreply.github.com>
2024-10-11 18:47:37 +0200

    Add files via upload
    actividaaaaa 33333

---
+++ Guillem_DehonFuentes_UF1_ACT3/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Guillem_DehonFuentes_UF1_ACT1"
+include(":app")
